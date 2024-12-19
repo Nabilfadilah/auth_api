ac57c43867c6b08eb1b4ab68c458cc34eab426c7
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-19 08:18:45 +0700

    Menambah route post login

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [ApiController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
