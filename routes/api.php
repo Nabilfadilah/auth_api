e0319c743bafb2d78eb5342aa55a34f5e6629add
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-19 08:49:15 +0700

    Menambah route get profile

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [ApiController::class, 'register']);
 Route::post('login', [ApiController::class, 'login']);
 
+Route::group([
+    "middleware" => ["auth:sanctum"]
+], function () {
+    //profile page
+    Route::get('profile', [ApiController::class, 'profile']);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
